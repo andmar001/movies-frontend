ebfaa7086d6fe8ee0003f719d4590830422b4d6c
tony tolver <andrademaresantonio00@gmail.com>
2023-05-14 01:32:50 -0600

    Mostrar las peliculas - diseño

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,32 +17,38 @@ export class AppComponent implements  OnInit {
           {
             titulo :'Spiderman',
             fechaLanzamiento: new Date(),
-            precio: 1400.99
+            precio: 1400.99,
+            poster: 'https://upload.wikimedia.org/wikipedia/commons/5/52/Spider-Man.jpg'
           },
           {
             titulo :'Moana',
             fechaLanzamiento: new Date('2016-11-14'),
-            precio: 1400.99
+            precio: 1400.99,
+            poster:'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Moana.svg/800px-Moana.svg.png'
           },
           {
             titulo :'Los Vengadores',
             fechaLanzamiento: new Date('2016-11-14'),
-            precio: 1400.99
+            precio: 1400.99,
+            poster:'https://www.altonivel.com.mx/wp-content/uploads/2019/05/avengers.jpg'
           },
           {
-            titulo :'Spiderman',
+            titulo :'Venom',
             fechaLanzamiento: new Date(),
-            precio: 1400.99
+            precio: 1400.99,
+            poster:'https://i.etsystatic.com/25769395/r/il/41899d/2757709903/il_fullxfull.2757709903_qkgk.jpg'
           },
           {
-            titulo :'Moana',
+            titulo :'Mario Bros',
             fechaLanzamiento: new Date('2016-11-14'),
-            precio: 1400.99
+            precio: 1400.99,
+            poster:'https://images.pexels.com/photos/163077/mario-yoschi-figures-funny-163077.jpeg?auto=compress&cs=tinysrgb&w=1600'
           },
           {
-            titulo :'Los Vengadores',
+            titulo :'Wall-e',
             fechaLanzamiento: new Date('2016-11-14'),
-            precio: 1400.99
+            precio: 1400.99,
+            poster:'https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/peliculas/wall-e/galeria/3139610-3-esl-ES/Wall-E.jpg'
           }
         ]
       }
--- src/assets/notes.md
+++ src/assets/notes.md
@@ -1,2 +1,14 @@
 # uso de ng container
 - ng-container es un elemento que no se renderiza en el DOM, pero que sirve para agrupar elementos y aplicarles directivas
+
+# extensiones
+- icono - fluent icons
+- prev image - image preview
+- version - version lens
+
+# rating component
+<app-rating (rated)="manajarRated($event)" ></app-rating>
+
+# referencia a un elemento del DOM
+<input type="file" style="display: none;" #cargador>
+<button (click)="cargador.click()">Cargar Archivo</button>
