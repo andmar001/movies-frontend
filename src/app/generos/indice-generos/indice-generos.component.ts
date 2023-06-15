8f9fc0930c76496e8d6249315952a9a5c70ca573
hp_computer <andrademareshp@gmail.com>
2023-06-15 00:51:34 -0600

    Mostrar listado de generos - Table

--- src/app/generos/indice-generos/indice-generos.component.ts
+++ src/app/generos/indice-generos/indice-generos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GenerosService } from '../generos.service';
+import { generoDTO } from '../genero';
 
 @Component({
   selector: 'app-indice-generos',
@@ -10,10 +11,14 @@ export default class IndiceGenerosComponent implements OnInit {
 
   constructor( private _generosService:GenerosService ){}
 
+  generos:generoDTO[]
+  //para tabla
+  columnasAMostrar = ['id','nombre','acciones']
+
   ngOnInit(): void {
     this._generosService.obtenerTodos()
       .subscribe(generos =>{
-        console.log(generos)
+        this.generos = generos;
       },
       error => console.log(error))
   }
